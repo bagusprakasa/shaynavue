17a4c557635fb7df3b19624eb2b24b9ac5972278
bagusprakasa <galihbagus555@gmail.com>
2022-02-16 23:00:50 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Banner />
+    <Insta />
+    <Logo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import Banner from "@/components/Banner.vue";
+import Insta from "@/components/Instagram.vue";
+import Logo from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    Banner,
+    Insta,
+    Logo,
+    Footer,
+  },
+};
+</script>
